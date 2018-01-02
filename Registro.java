01d052086cd07a8dc82b63e32acefbb94f63fe5c
VitorGiam <vitor.giam@hotmail.com>
2018-01-02 11:02:39 -0200

    Update Registro.java

--- Registro.java
+++ Registro.java
@@ -1,10 +1,6 @@
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
-/**
- *
- * @author Kakugawa
- */
 public interface Registro extends Remote{
     
     public Resposta insert(Aluno aluno) throws RemoteException;
